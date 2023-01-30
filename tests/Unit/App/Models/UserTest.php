61889388e23bf380a221c3603e52db32269cfc77
Matheus-O-Silva <matheus.oliveira110101@gmail.com>
2023-01-30 18:24:18 -0300

    first commit

---
+++ tests/Unit/App/Models/UserTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit\App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    protected function model(): User
+    {
+        return new User();
+    }
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_traits()
+    {
+        $traits = array_keys(class_uses($this->model()));
+
+        $expectedTraits = [
+            HasApiTokens::class,
+            HasFactory::class,
+            Notifiable::class
+        ];
+
+        $this->assertEquals($expectedTraits, $traits);
+    }
+}
