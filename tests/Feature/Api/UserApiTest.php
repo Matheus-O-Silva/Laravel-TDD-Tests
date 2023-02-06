7d99d1dcc19288d4e0968642f2e7004fe37ae096
Matheus-O-Silva <matheus.oliveira110101@gmail.com>
2023-02-06 11:41:18 -0300

    testing json structure

--- tests/Feature/Api/UserApiTest.php
+++ tests/Feature/Api/UserApiTest.php
@@ -16,6 +16,18 @@ class UserApiTest extends TestCase
     {
         $response = $this->getJson($this->endpoint);
         $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonCount(0,'data');
+        $response->assertJsonStructure([
+            'meta' => [
+                'total',
+                'current_page',
+                'last_page',
+                'first_page',
+                'per_page'
+            ]
+        ]);
+
+        $this->assertEquals(0, $response['meta']['total']);
     }
 
     public function test_paginate()
@@ -23,8 +35,21 @@ class UserApiTest extends TestCase
         User::factory()->count(40)->create();
 
         $response = $this->getJson($this->endpoint);
+
         $response->assertStatus(Response::HTTP_OK);
         $response->assertJsonCount(15,'data');
+        $response->assertJsonStructure([
+            'meta' => [
+                'total',
+                'current_page',
+                'last_page',
+                'first_page',
+                'per_page'
+            ]
+        ]);
+
+        $this->assertEquals(40, $response['meta']['total']);
+        $this->assertEquals(1, $response['meta']['current_page']);
     }
 
     public function test_page_two()
